f0cc2f151e144e7628a11bae7f1d35cc6bb645d4
Jireh Liceralde <jireh.liceralde@gmail.com>
2020-05-18 21:36:47 -0500

    Created JobField abstract class and extended it to Employer, Location, PositionType, and CoreCompetency classes

--- src/org/launchcode/techjobs_oo/Location.java
+++ src/org/launchcode/techjobs_oo/Location.java
@@ -2,33 +2,15 @@ package org.launchcode.techjobs_oo;
 
 import java.util.Objects;
 
-public class Location {
-    private int id;
-    private static int nextId = 1;
-    private String value;
-
-    public Location() {
-        id = nextId;
-        nextId++;
-    }
-
-    // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
-    //  constructor should also call the empty constructor in order to initialize the 'id' field.
+public class Location extends JobField {
 
     public Location (String value) {
 
-        this();
-        this.value = value;
+        super(value);
 
     }
 
-
-    // Custom toString, equals, and hashCode methods:
-
-    @Override
-    public String toString() {
-        return value;
-    }
+    // Custom equals, and hashCode methods:
 
     @Override
     public boolean equals(Object o) {
@@ -43,17 +25,4 @@ public class Location {
         return Objects.hash(getId());
     }
 
-    // Getters and Setters:
-
-    public int getId() {
-        return id;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
 }
